c6bdf3a37f80e789bdbe8efb4f3bca1f4b8c67fc
ARIBİLGİ <ARIBLG@PC1024>
2023-02-19 14:02:32 +0300

    Koşul Yapıları bitti.

--- Ders04Operatorler/Program.cs
+++ Ders04Operatorler/Program.cs
@@ -34,7 +34,7 @@ Console.WriteLine("b--: " + b);
 
 Console.WriteLine(); //boş satır için.
 
-Console.WriteLine("1- Atama Operatörleri!");
+Console.WriteLine("2- Atama Operatörleri!");
 
 
 Console.WriteLine("a'nın değeri: " + a );
@@ -44,6 +44,40 @@ Console.WriteLine("a'nın değeri: " + c );
 Console.WriteLine("a += b: " + (a += b)); // a'nın değerini b kadar arttır demektir.
 
 Console.WriteLine("a = a + b: " + (a = a + b)); // bu işlem ile a+=b işlemi aynı işlevi görür.
+
+
+Console.WriteLine("a -= b: " + (a -= b)); // a'nın değerini b kadar azalt demektir.
+Console.WriteLine("a *= b: " + (a *= b)); // a'nın değerini b ile çarpar.
+Console.WriteLine("a /= b: " + (a /= b)); // a'nın değerini b ile böler.
+
+Console.WriteLine("3- İlişkisel Operatörler!");
+
+Console.WriteLine($"a({a}) == b({b}) : " + (a==b)); // a, b'ye eşit mi?
+
+Console.WriteLine($"a({a}) != b({b}): " + (a!=b)); // a, b'ye eşit değil mi?
+
+Console.WriteLine($"a({a}) > b({b}): " + (a>b)); // a, b'den büyük mü?
+
+Console.WriteLine($"a({a}) < b({b}): " + (a < b)); //a, b'den küçük mü?
+
+Console.WriteLine($"a({a}) <= b({b}): " + (a <= b)); //a, b'den küçük veya eşit mi?
+
+Console.WriteLine($"a({a}) >= b({b}): " + (a >= b)); //a, b'den büyük veya eşit mi? 
+
+// $ işareti string alanları içerisinde yazılımsal değerleri süslü parantez kullanarak yazdırmaya yarıyor. 
+// ? işareti ise ternary dir. iki olasılıktan birini yazdırır.
+
+Console.WriteLine("ternary operatörü a == b: " + ((a == b) ? "a, b'ye eşit" : "a, b'ye eşit değil")); //2 olasılıklı durumlarda kullanabileceğimiz ternary operatörü.
+
+Console.WriteLine(); //boşluk bırakalım.
+Console.WriteLine("3- Mantıksal Operatörler!");
+Console.WriteLine($"a-{a}, b-{b}, c-{c} + ((a > b) && ( b> c)): " + ((a > b) && (b > c))); // && - ve operatörüdür. İki şartın doğruluğunu kontrol eder.
+
+Console.WriteLine($"a-{a}, b-{b}, c-{c} + ((a > b) || ( b> c)): " + ((a > b) || (b < c))); // || - veya operatörüdür. İki şarttan herhangi birinin doğruluğuna bakar.
+
+Console.WriteLine(); //boşluk bırakalım.
+Console.WriteLine($"a-{a}, b-{b} (a > b) : " + (a > b));  // burada a, b'den büyük mü bakıyoruz. 
+Console.WriteLine($"a-{a}, b-{b} (a > b) : " + !(a > b)); // ! iaşreti ile bir işlem sonucunun tam tersini elde ederiz.
 
 
 
---
+++ Ders05KosulYapilari/Program.cs
@@ -0,0 +1,89 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Koşul Yapıları!");
+
+int sayi = -10;
+
+Console.WriteLine("Lütfen Bir Sayı Giriniz: ");
+sayi = Convert.ToInt32(Console.ReadLine()); //İnput girişi. Ekrandan veri alabilmek için Readline metodu kullanılır. ReadLine metodu ekrandan verileri string olarak alır ve sayısal işlem yapılacaksa convert(dönüştürmek) etmek gerekir. Casting metodu ile "(int)" şeklinde de çevrilebilir fakat program bazen kabul etmez, bu yüzden convert metodu kullandık burada.
+
+
+if (sayi > 0) //eğer sayı 0'dan büyükse
+{
+    Console.WriteLine("Sayı Pozitif");
+}
+
+else if (sayi < 0)
+{
+    Console.WriteLine("Sayı Negatif");
+}
+
+else
+{
+    Console.WriteLine("Sayı 0");
+}
+
+Console.WriteLine(); //Bir satır boşluk
+Console.WriteLine("Kullanıcı Adını Giriniz: ");
+
+string kullaniciadi = Console.ReadLine();
+
+Console.WriteLine("Şifrenizi Giriniz: ");
+
+string sifre = Console.ReadLine();
+
+if (kullaniciadi == "admin" && sifre == "123")
+{
+    Console.WriteLine("Giriş Başarılı! \n Hoşgeldin!: "+ kullaniciadi);
+}
+else
+{
+    Console.WriteLine("Giriş Başarısız!");
+}
+
+int saat = DateTime.Now.Hour; // DateTime ile tarih ve saat bilgilerine ulaşırız.(.NET kütüphanesinden)
+if (saat > 19)
+    Console.WriteLine("İyi Akşamlar!"); //tek satırlı kodlarda süslü parantez kullanılmayabilir.
+
+else
+    Console.WriteLine("Saat:"+ saat +" İyi Günler!");
+
+Console.WriteLine();
+
+Console.WriteLine("Ternary Operatörü"); //bu operatör ile tek satırla da halledilebilir.
+
+Console.WriteLine((saat > 19)? "Saat:" + saat + " İyi Akşamlar!" : "Saat:" + saat + " İyi Günler!"); //ternary operatörü ile tek satır da halledilebilir.
+
+Console.WriteLine();
+
+Console.WriteLine("Switch Case Kullanımı");
+
+int ay = DateTime.Now.Month; //bulunduğumuz ay değerine ulaştık.
+
+switch (ay)  //switch de parantez içi kontrol edilecek alandır. (Deneme yanılma)
+{
+    case 12:
+    case 1:
+    case 2:
+        Console.WriteLine("Mevsim Kış");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("Mevsim İlkbahar");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        Console.WriteLine("Mevsim Yaz");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        Console.WriteLine("Mevsim Sonbahar");
+        break;
+
+    default:
+        Console.WriteLine("Hiçbir şart uymadıysa deault çalışır.");
+        break;
+}
+
