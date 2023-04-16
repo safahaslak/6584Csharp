d70028bd63544c50c92b20974d360d2e5a7c0fc1
ARIBİLGİ <ARIBLG@PC1024>
2023-04-16 14:02:45 +0300

    16-04-2023 Css arka planlar bitti

---
+++ WebApplicationEgitim/WebFormElementleri.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationEgitim
+{
+    public partial class WebFormElementleri : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // windows forms daki form açılırken db den verileri çekip ekrana yollamak için kullanıdığımız form load metodu web de pageload olarak aynı işlem için kullanılıyor.
+            // Bu kısımda win forms daki code behind işlemleri yapılır CRUD vb.
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text = "Butona Tıklandı!";
+            Label1.Text += "DropdownList'den seçilen değer : SelectedValue : " + DropDownList1.SelectedValue + " - SelectedItem.Text :" + DropDownList1.SelectedItem.Text; 
+
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            HiddenField1.Value = "HiddenField elementi sayfada gizli veri tutar.";
+        }
+    }
+}
